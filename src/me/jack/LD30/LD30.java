221444e4b80b85614b72f8c6dfaeeb5eef56c42e
Jack Patrick <jackpatrickmail@gmail.com>
2014-08-22 18:27:30 +0100

    Base code added.

---
+++ src/me/jack/LD30/LD30.java
@@ -0,0 +1,20 @@
+package me.jack.LD30;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * Created by Jack on 22/08/2014.
+ */
+public class LD30 extends StateBasedGame {
+
+    public LD30(String name) {
+        super(name);
+    }
+
+    @Override
+    public void initStatesList(GameContainer gameContainer) throws SlickException {
+
+    }
+}
---
+++ src/me/jack/LD30/Main.java
@@ -0,0 +1,13 @@
+package me.jack.LD30;
+
+import uk.co.jdpatrick.JEngine.JEngine;
+
+/**
+ * Created by Jack on 22/08/2014.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        JEngine.start(new LD30("NAME for LD30 --- Theme:Theme"),800,600);
+    }
+}
