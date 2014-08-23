c9eecb523e3a8d256cc86fdd155e0a114c44280f
Jack Patrick <jackpatrickmail@gmail.com>
2014-08-23 03:38:13 +0100

    Added a perlin noise generator, will be used to create levels.

--- src/me/jack/LD30/Level/LevelGenerator.java
+++ src/me/jack/LD30/Level/LevelGenerator.java
@@ -12,15 +12,15 @@ public class LevelGenerator {
 
 
 
-    public static float[][] generateWhiteNoise(int width, int height){
+    public static float[][] generateWhiteNoise(int width, int height,Random r){
 
-        Random r = new Random();
+
 
         float[][] noise = new float[width][height];
 
         for(int i = 0;i < width;i++){
             for(int j = 0;j < height;j++){
-                noise[i][j] = (float)r.nextDouble() % 1;
+                noise[i][j] = (float)r.nextDouble();
             }
         }
 
@@ -39,46 +39,134 @@ public class LevelGenerator {
         int sP = 1 << octave;
         float sF = 1.0f/sP;
 
-        for(int j = 0;j < w;j++){
+        for(int i = 0;i < w;i++){
 
-            int s_j0 = (j/sP)*sP;
-            int s_j1 = (s_j0 + sP) % w;
+            int s_i0 = (i/sP)*sP;
+            int s_i1 = (s_i0 + sP) % w;
+            float hB = (i-s_i0) * sF;
 
-            float vB = (j- s_j0) * sF;
+            for(int j = 0; j < h;j++){
 
-            float top =
+                int s_j0 = (j/sP) * sP;
+                int s_j1 = (s_j0 + sP) % h;
+                float vB = (j-s_j0) *sF;
+
+                float top = interpolate(noise[s_i0][s_j0],noise[s_i1][s_j0],hB);
+
+                float bottom = interpolate(noise[s_i0][s_j1],noise[s_i1][s_j1],hB);
+
+                smoothNoise[i][j] = interpolate(top,bottom,vB);
+            }
+        }
+        return smoothNoise;
+    }
+
+
+    public static float[][] generatePerlinNoise(float[][] noise, int oC){
+        int w = noise.length;
+        int h = noise[0].length;
+
+        float[][][] smoothNoise = new float[oC][][];
+
+        float persistence = 0.5f;
+
+        for(int i = 0;i < oC;i++){
+            smoothNoise[i] = generateSmoothNoise(noise,i);
         }
 
+        float[][] perlinNoise = new float[w][h];
+
+        float amplitude = 1.0f;
+        float totalAmplitude = 0.0f;
+
+        for(int octave = oC - 1;octave >0;octave--){
+            amplitude *=persistence;
+            totalAmplitude+=amplitude;
+
+            for(int i = 0;i < w;i++){
+                for(int j = 0;j < h;j++){
+                    perlinNoise[i][j] += smoothNoise[octave][i][j] * amplitude;
+                }
+            }
+        }
+
+
+        for(int i = 0;i < w;i++){
+            for(int j = 0;j < h;j++){
+                perlinNoise[i][j] /=totalAmplitude;
+            }
+        }
+    return perlinNoise;
+    }
+
+
+
+    public static float interpolate(float x0,float x1,float alpha){
+        return x0*(1-alpha) + alpha * x1;
     }
 
 
 
 
 
-
-
     public static void main(String[] args) {
+        Random r = new Random();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         while (true) {
+
+            float[][] whiteNoise = generateWhiteNoise(50,50,r);
+
+            float[][] perlin = generatePerlinNoise(whiteNoise,6);
+
             int[] noise = null;
 
-            BufferedImage image = new BufferedImage(20, 20, BufferedImage.TYPE_INT_RGB);
+            BufferedImage image = new BufferedImage(50, 50, BufferedImage.TYPE_INT_RGB);
 
-            int[] pixels = new int[20 * 20];
+            int[] pixels = new int[50*50];
 
-            for (int i = 0; i != noise.length; i++) {
-                int n = noise[i];
-                if (n == 0) {
-                    pixels[i] = 0xffffff;
-                } else {
-                    pixels[i] = 0x000080;
+            for(int x = 0;x!=perlin.length;x++){
+                for(int y = 0;y!= perlin[0].length;y++){
+                    float p = perlin[x][y];
+
+                    Color c = getColor(Color.black,Color.DARK_GRAY,p);
+                    pixels[x+y*50] = c.hashCode();
                 }
             }
 
-            image.setRGB(0, 0, 20, 20, pixels, 0, 20);
+            image.setRGB(0, 0, 50, 50, pixels, 0, 50);
 
-            JOptionPane.showMessageDialog(null, null, "Test", JOptionPane.YES_NO_OPTION, new ImageIcon(image.getScaledInstance(20 * 4, 20 * 4, Image.SCALE_AREA_AVERAGING)));
+            JOptionPane.showMessageDialog(null, null, "Test", JOptionPane.YES_NO_OPTION, new ImageIcon(image.getScaledInstance(50*4,50*4,0)));
         }
 
+    }
+
+
+
+    public static Color getColor(Color s,Color e, float t){
+        float u = 1-t;
+
+
+        int r = (int) (s.getRed() * u + e.getRed() * t);
+        int g =  (int) (s.getGreen() * u + e.getGreen() * t);
+        int b =  (int) (s.getBlue() * u + e.getBlue() * t);
+
+
+        Color c = new Color(r,g,b);
+        return c;
     }
 
 }
